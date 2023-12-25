24d109ed55844aae32f4392c010de6b7ec01ddf7
akhil3046 <100361924+akhil3046@users.noreply.github.com>
2023-12-25 13:08:02 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ import "./Home.css"; // Create a separate CSS file for Home component styles
 function Home() {
   const navigate = useNavigate();
   const [restaurants, setRestaurants] = useState([]);
-  const URL = 'https://server-y2dt.onrender.com/'
+  const URL = 'https://server-y2dt.onrender.com'
   const handleButton = () => {
     navigate("/restaraunt");
   };
