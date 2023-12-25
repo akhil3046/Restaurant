84b761dae95f0afb51451b2ec20787d95441d60b
akhil3046 <100361924+akhil3046@users.noreply.github.com>
2023-12-25 13:05:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,15 @@ const app = express();
 mongoose.connect("mongodb+srv://Ajay-kumar:Ajaykumar$13@cluster0.ofmxz.mongodb.net/akhilcsd");
 // app.set("trust proxy", 1);
 app.use(cors({
-    origin:'https://restaurant-mkn3.onrender.com/',
+    origin:'*',
     credentials:true,
 }));
+app.options("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://restaurant-mkn3.onrender.com/");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(204);
+});
 app.use(express.json());
 app.use(bodyParser.json());
 
